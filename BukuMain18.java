3b2a366ca98c889ccd392cc1cac0eab609d489c9
MariskaPutri18 <mariskaputri960@gmail.com>
2024-02-27 19:42:19 +0700

    latihan 1

--- BukuMain18.java
+++ BukuMain18.java
@@ -11,15 +11,34 @@ public class BukuMain18 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
-
+       
+        double totalHarga = bk1.hitungHargaTotal(5);
+        System.out.println("Harga Total: Rp " + totalHarga);
+        double diskon = bk1.hitungDiskon((int) totalHarga, bk1.harga);
+        System.out.println("Diskon Buku : Rp " + diskon);
+        double hargabayar = bk1.hitungHargaBayar((int) totalHarga, (int) diskon);
+        System.out.println("Total harga bayar : Rp " + hargabayar);
+       
         Buku bk2 = new Buku ("Self Reward", "Maheera ayesha", 160, 29, 59000);
         bk2.terjual(11);
         bk2.tampilInformasi();
-
+        double totalHarga1 = bk2.hitungHargaTotal(11);
+        System.out.println("Harga Total: Rp " + totalHarga1);
+        double diskon1 = bk2.hitungDiskon((int) totalHarga1, bk2.harga);
+        System.out.println("Diskon Buku : Rp " + diskon1);
+        double hargabayar1= bk2.hitungHargaBayar((int) totalHarga1, (int) diskon1);
+        System.out.println("Total harga bayar : Rp " + hargabayar1);
+       
         Buku bukuMahasiswa = new Buku("Laut Berceria", "Laila S.Chudori", 379, 5, 89000);
         bukuMahasiswa.terjual(2);
         bukuMahasiswa.tampilInformasi();
-
+        double totalHarga2 = bukuMahasiswa.hitungHargaTotal(2);
+        System.out.println("Harga Total: Rp " + totalHarga2);
+        double diskon2 = bukuMahasiswa.hitungDiskon((int) totalHarga2, bukuMahasiswa.harga);
+        System.out.println("Diskon Buku : Rp " + diskon2);
+        double hargabayar2 = bukuMahasiswa.hitungHargaBayar((int) totalHarga2, (int) diskon2);
+        System.out.println("Total harga bayar : Rp " + hargabayar2);
+       
       }      
 }
 
@@ -46,7 +65,27 @@ class Buku {
     void gantiHarga(int hrg) {
         harga = hrg;
     }
-    
+
+    int hitungHargaTotal(int jml) {
+        int hargaTotal = jml * harga;
+        return hargaTotal;
+    }
+
+    int hitungDiskon(int hargaTotal, int harga){
+        int diskon = 0;
+        if (hargaTotal >150000) {
+            diskon = (int) (harga * 0.12);  
+        }else if (hargaTotal > 75000 && hargaTotal < 150000  ){
+            diskon = (int) (harga * 0.15);
+        }
+        return diskon;
+    }
+
+    int hitungHargaBayar(int hargaTotal, int diskon){
+        int hargabayar= hargaTotal- diskon;
+        return hargabayar;
+
+    }
     public Buku(){
 
     }
